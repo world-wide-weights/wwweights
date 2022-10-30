fa068effe18fd3a239346c73fe35edffd4b1b466
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-10-30 14:50:33 +0100

    Add Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('World Wide Weights Api')
+    .setDescription('The wwweights Api overview')
+    .setVersion('0.1')
+    .addTag('items')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
