d9abdf046e3868ca1909ddd04128b21d3e485a28
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-10-30 14:52:09 +0100

    Add example endpoints for items

--- backend/src/items/items.service.ts
+++ backend/src/items/items.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
+  constructor(
+    @InjectRepository(Item)
+    private itemsRepository: Repository<Item>,
+  ) {}
+
   create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+    const testItem = {
+      name: 'test',
+      weight: 'test',
+      tags: ['test', 'test2'],
+      image: 'test',
+      user: 'test',
+    };
+    const item = this.itemsRepository.create(testItem);
+    return this.itemsRepository.save(item);
   }
 
   findAll() {
-    return `This action returns all items`;
+    return this.itemsRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} item`;
+    return this.itemsRepository.findOneBy({ id });
   }
 
   update(id: number, updateItemDto: UpdateItemDto) {
