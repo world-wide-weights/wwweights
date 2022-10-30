1e9eddd5124c64d52c57cbeeac33cdc138289a5c
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-10-30 14:51:01 +0100

    Add app.module TypeOrm Config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
+import { Item } from './items/entities/item.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [Item],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
