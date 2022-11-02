5c3ec7b570a537bd5f01e42f00376a0be91f19e9
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-11-02 21:58:21 +0100

    Add Global Pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('World Wide Weights Api')
     .setDescription('The wwweights Api overview')
