7b883fe9c423bf5dddd9b483857204d7a125814e
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-11-02 21:58:56 +0100

    Add Dto and plainToInstance Decorators

--- backend/src/items/entities/item.entity.ts
+++ backend/src/items/entities/item.entity.ts
@@ -1,3 +1,5 @@
+import { Expose } from 'class-transformer';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -5,21 +7,35 @@ export class Item {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsString()
+  @Expose()
   @Column()
   name: string;
 
+  @IsString()
+  @Expose()
   @Column()
-  weight: string; // exact - rannge - ca.
+  weight: string; // exact - range - ca.
 
+  @IsString({ each: true })
+  @Expose()
   @Column('text', { array: true })
   tags: string[];
 
+  @IsString()
+  @Expose()
   @Column()
   image: string; // Link to static store or base-64 Encoded
 
+  @IsOptional()
+  @IsString()
   @Column()
+  @Expose({ groups: ['admin'] })
   user?: string;
 
+  @IsBoolean()
+  @Expose()
   @Column({ default: true })
+  @Expose({ groups: ['admin'] })
   isActive: boolean;
 }
