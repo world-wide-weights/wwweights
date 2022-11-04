941b714851a899be247e4081c85b904300943838
Lars Flieger <a.purenetwork@gmail.com>
2022-11-04 17:03:14 +0100

    Update next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
