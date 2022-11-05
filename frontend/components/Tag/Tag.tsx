4a855611da4da38f4eb6bf3bf77ff2041c32eeec
Marvin Pfau <marvin.pfau@stihl.de>
2022-11-05 16:06:34 +0100

    Add Tag component

--- frontend/components/Tag/Tag.tsx
+++ frontend/components/Tag/Tag.tsx
@@ -1,13 +1,33 @@
+
+export enum TagTypesEnum {
+    red = 'red-600',
+    purple = "indigo-500",
+    pink = "pink-500",
+    lightblue = "primary-blue-hover-icon",
+    blue = "primary-blue",
+    green = "emerald-500",
+    yellow = "amber-500",
+    lightgrey = "lightgrey",
+    darkgrey = "darkgrey",
+    dark = "gray-900",
+}
+
 type TagProps = {
     /** Content of tag */
-   children: React.ReactNode
+    title: String
+
+    /** href destination Link */
+    link: String
+
+    /** Possibility to change color and background of tag */
+    type?: TagTypesEnum
 }
 
 /**
- * Chip
+ * Tag
  */
-export const Tag: React.FC<TagProps> = ({ children }) => {
+export const Tag: React.FC<TagProps> = ({ title, link, type = TagTypesEnum.blue }) => {
     return (
-        <p>{children}</p>
+        <a href="{link}" className="inline-block bg-${type} bg-opacity-20 text-${type} rounded-full px-5 py-1 mr-2 mb-2">{title}</a>
     )
 }
