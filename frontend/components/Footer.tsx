a870823985fb01bb33c2712740a60a1bd7441393
Marvin Pfau <marvin.pfau@stihl.de>
2022-11-06 13:05:27 +0100

    Add Copyright Footer

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,14 @@
+/**
+ * Footer
+ */
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="py-1 bg-gray-200 text-center">
+        <p className="text-center text-base text-gray-500">
+          © WWWeights {new Date().getFullYear()} -<a href=""> Imprint</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
