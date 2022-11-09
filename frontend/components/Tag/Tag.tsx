e6962c5c3ccc84ced9ca157a2fc7b44a937e14d7
Marvin Pfau <marvin.pfau@stihl.de>
2022-11-09 15:31:06 +0100

    Add Color for text, changed safelist, names

--- frontend/components/Tag/Tag.tsx
+++ frontend/components/Tag/Tag.tsx
@@ -1,30 +1,46 @@
-export enum TagTypesEnum {
-    red = 'red-500',
-    purple = "indigo-500",
-    pink = "pink-500",
-    cyan = "cyan-500",
-    blue = "blue-500",
-    green = "emerald-500",
-    yellow = "amber-500",
-    grey = "gray-500",
-}
-
 type TagProps = {
-    /** Content of tag */
-    children: React.ReactNode
+  /** Content of tag */
+  children: React.ReactNode;
 
-    /** href destination Link */
-    link: String
+  /** href destination Link */
+  to: String;
 
-    /** Possibility to change color and background of tag */
-    type?: TagTypesEnum
-}
+  /** Possibility to change color and background of tag */
+  color?:
+    | "red"
+    | "indigo"
+    | "pink"
+    | "cyan"
+    | "blue"
+    | "emerald"
+    | "amber"
+    | "gray"
+    | "slate"
+    | "zinc"
+    | "neutral"
+    | "stone"
+    | "orange"
+    | "yellow"
+    | "lime"
+    | "green"
+    | "teal"
+    | "sky"
+    | "violet"
+    | "purple"
+    | "fuchsia"
+    | "rose";
+};
 
 /**
  * Tag
  */
-export const Tag: React.FC<TagProps> = ({ children, link, type = TagTypesEnum.blue }) => {
-    return (
-        <a href={`${link}`} className={`inline-block bg-${type} bg-opacity-30 rounded-full px-5 py-1 mr-2 mb-2`}>{children}</a>
-    )
-}
+export const Tag: React.FC<TagProps> = ({ children, to, color }) => {
+  return (
+    <a
+      href={`${to}`}
+      className={`inline-block bg-${color}-500 bg-opacity-30 text-${color}-800 rounded-full px-5 py-1 mr-2 mb-2`}
+    >
+      {children}
+    </a>
+  );
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}'
-  ],
-  safelist: [{
-      pattern: /(bg)-*/,
-    }
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  safelist: [
+    {
+      pattern: /bg-(.*)-500/,
+    },
+    {
+      pattern: /text-(.*)-800/,
+    },
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
