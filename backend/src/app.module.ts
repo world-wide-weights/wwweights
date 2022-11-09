2f60773397c2a7fde72d6e43202c39525723acdb
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-11-10 00:20:41 +0100

    Work in progress fixing db connection in docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ItemsModule } from './items/items.module';
 import { Item } from './items/models/item.model';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.development.env',
+      envFilePath: '../.env',
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
+        host: 'localhost', //configService.get('DB_HOST'),
+        //port: +'5432', //configService.get('DB_PORT'),
+        username: 'admin', //configService.get('DB_USER'),
+        password: 'admin', //configService.get('DB_PW'),
+        database: 'wwweights', //configService.get('DB_DB'),
         entities: [Item],
         synchronize: true,
       }),
