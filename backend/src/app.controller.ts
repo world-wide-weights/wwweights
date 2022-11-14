028db55d6c33cbc9b9ef9774dbdd63cd8888efef
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-11-14 11:23:27 +0100

    devops(setup): remove required semicolons

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
@@ -8,6 +8,6 @@ export class AppController {
   @Get()
   getHello(): string {
     // Test123
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
