353e748e8bb0712db91cbac55a4ccee2f1788ccc
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-11-14 19:40:15 +0100

    Add Logger, try catch and Swagger to controller

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -2,12 +2,14 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Get,
+  HttpStatus,
+  Logger,
   Param,
   Post,
   UseInterceptors,
 } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GetItemDto } from './interfaces/get-item-dto';
@@ -18,6 +20,8 @@ import { GetItemQuery } from './queries/impl';
 @ApiTags('items')
 @UseInterceptors(ClassSerializerInterceptor)
 export class ItemsController {
+  private readonly logger = new Logger(ItemsController.name);
+
   constructor(
     private queryBus: QueryBus,
     // just here for Testing since no create issue was created
@@ -28,16 +32,20 @@ export class ItemsController {
   @Post()
   createItem() {
     // This is currently jsut for testing since it is not part of any issue
-    const newItem = new Item({
-      name: 'test Name with SpAcEs ',
-      value: '1kg',
-      is_ca: true,
-      tags: ['testTag', 'testTag2'],
-      user: 'testUser',
-      isActive: true,
-    });
-    newItem.applySlug();
-    return this.repository.save(newItem);
+    try {
+      const newItem = new Item({
+        name: 'test Name with SpAcEs ',
+        value: '1kg',
+        is_ca: true,
+        tags: ['testTag', 'testTag2'],
+        user: 'testUser',
+        isActive: true,
+      });
+      newItem.applySlug();
+      return this.repository.save(newItem);
+    } catch (error) {
+      // TODO: ExceptionsHandler still logs the error even after catching, no need to do error handling?
+    }
   }
 
   @Get()
@@ -46,7 +54,20 @@ export class ItemsController {
   }
 
   @Get(':id')
-  async getItem(@Param() { id }: GetItemDto) {
-    return this.queryBus.execute(new GetItemQuery(id)); // TODO Check if this i converted to Number with dto and Serializer
+  @ApiParam({ name: 'id', type: Number })
+  @ApiOperation({ summary: 'Get an item by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The found item',
+    type: Item,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'No item found',
+    type: null,
+  })
+  async getItem(@Param() id: GetItemDto) {
+    this.logger.log(`Get item with id ${id}`);
+    return this.queryBus.execute(new GetItemQuery(id.id)); // TODO Check if this i converted to Number with dto and Serializer
   }
 }
