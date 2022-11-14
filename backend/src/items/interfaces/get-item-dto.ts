78ccf85f09ee7f42510093ef3bd89d0aa1e1592f
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-11-14 19:40:34 +0100

    Type cast because no impicitConversion!

--- backend/src/items/interfaces/get-item-dto.ts
+++ backend/src/items/interfaces/get-item-dto.ts
@@ -1,7 +1,9 @@
+import { Type } from 'class-transformer';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class GetItemDto {
   @IsNumber()
+  @Type(() => Number)
   @IsNotEmpty()
   id: number;
 }
