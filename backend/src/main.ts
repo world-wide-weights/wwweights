bdd89634e1d9dc22f44ac4e51e6177141da22f6a
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-11-17 01:22:14 +0100

    Add whitelist pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Swagger
   const config = new DocumentBuilder()
