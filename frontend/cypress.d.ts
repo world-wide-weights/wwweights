7b531e461d9b8bd13ee4f32705275108dff7750c
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-11-20 14:47:26 +0100

    fix(build): setup cypress properly

---
+++ frontend/cypress.d.ts
@@ -0,0 +1,12 @@
+// Code stolen from cypress docs
+// https://docs.cypress.io/api/commands/mount#Creating-a-New-cy-mount-Command
+
+import { mount } from 'cypress/react'
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount
+    }
+  }
+}
