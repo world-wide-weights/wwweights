5cfdada1b0e36ee530ea9b90a2ac6a69f63d359d
Lars Flieger <a.purenetwork@gmail.com>
2022-11-25 15:12:28 +0100

    Update base strucutre of button

--- frontend/components/Button/Button.tsx
+++ frontend/components/Button/Button.tsx
@@ -28,15 +28,20 @@ export const Button: React.FC<ButtonProps> = ({ kind = "primary", disabled, icon
     // When loading should be disabled
     disabled = loading ? true : disabled
 
-    // Tailwind Classes 
+    // Base Classes 
+    const buttonBaseClasses = "flex items-center justify-center md:justify-start font-semibold border border-transparent rounded-full py-2 px-8 w-full md:w-max"
+    const linkBaseClasses = "flex items-center text-gray-600 font-semibold"
+
+    // Disable state classes
     const disabledClassesPrimarySecondary = "text-opacity-75 opacity-80 cursor-default"
     const disabledPrimary = disabled ? disabledClassesPrimarySecondary : "hover:bg-blue-600 focus:bg-blue-700"
     const disabledSecondary = disabled ? disabledClassesPrimarySecondary : "hover:bg-blue-500 focus:bg-blue-600 hover:text-white focus:text-white"
     const disabledTertiary = disabled ? disabledClassesPrimarySecondary : "hover:text-gray-800 focus:text-gray-900"
-    const buttonBaseClasses = "flex items-center justify-center md:justify-start font-semibold border border-transparent rounded-full py-2 px-8 w-full md:w-max"
+
+    // Kind classes
     const primaryClasses = `bg-blue-500 text-white ${disabledPrimary}`
     const secondaryClasses = `border border-blue-500 text-blue-500 ${disabledSecondary}`
-    const linkBaseClasses = "flex items-center text-gray-600 font-semibold"
+
     const innerContent = <>
         {icon && (loading ? <i className={`material-symbols-rounded text-xl mr-1 ${loading ? "animate-spin" : ""}`}>sync</i> : <i className="material-symbols-rounded text-xl mr-1">{icon}</i>)}
         {children}
