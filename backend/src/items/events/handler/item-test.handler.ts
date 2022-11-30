5836e02e4cdedfbaed7e90b4fe52a7689423d842
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-12-01 00:13:13 +0100

    comment

--- backend/src/items/events/handler/item-test.handler.ts
+++ backend/src/items/events/handler/item-test.handler.ts
@@ -4,6 +4,7 @@ import { ItemTestEvent } from '../impl/item-test.event';
 @EventsHandler(ItemTestEvent)
 export class ItemTestHandler implements IEventHandler<ItemTestEvent> {
   handle(event: ItemTestEvent) {
+    // TODO: Wtf to do here!?
     console.log('ItemTestHandler', event);
   }
 }
