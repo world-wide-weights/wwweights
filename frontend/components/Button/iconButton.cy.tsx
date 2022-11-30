6ba4acb7a29e29a3fdb38cb8ef7d55cdea229240
Marvin <87226274+mlp909@users.noreply.github.com>
2022-11-30 17:37:49 +0100

    add first tests

--- frontend/components/Button/iconButton.cy.tsx
+++ frontend/components/Button/iconButton.cy.tsx
@@ -2,10 +2,21 @@ import '../../styles/global.css';
 import 'material-symbols';
 import { IconButton } from "./IconButton";
 
-describe('iconButton.cy.ts', () => {
-  it('should display component', () => {
-    cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
-  })
+describe('IconButton', () => {
+    describe('iconButton.cy.ts', () => {
+        it('should display component', () => {
+          cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
+          cy.get('button').should('have.class', 'cursor-pointer hover:bg-gray-100 focus:bg-gray-200 rounded-full w-10 h-10 flex items-center justify-center')
+        })
+      })
+
+    describe('Icon', () => {
+        it('should display icon', () => {
+            cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
+            cy.get('button i').should('be.visible')
+        })
+    })
 })
+
 
 export { };
