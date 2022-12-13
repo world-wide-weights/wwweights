02daa25b5b884c06b32f5e5fe1b959f04695a5b2
Marvin <87226274+mlp909@users.noreply.github.com>
2022-12-13 09:18:56 +0100

    updated tests

--- frontend/components/Button/iconButton.cy.tsx
+++ frontend/components/Button/iconButton.cy.tsx
@@ -6,7 +6,7 @@ describe('IconButton', () => {
     describe('Button with onClick', () => {
         it('should display component', () => {
           cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
-          cy.get('button').should('have.class', 'cursor-pointer hover:bg-gray-100 focus:bg-gray-200 rounded-full w-10 h-10 flex items-center justify-center')
+          cy.get('button')
         })
         it('should display icon', () => {
           cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
@@ -17,7 +17,11 @@ describe('IconButton', () => {
     describe('Button as link', () => {
       it('should display component', () => {
         cy.mount(<IconButton icon='menu' to=''/>)
-        cy.get('button').should('attr', 'onClick')
+        cy.get('button')
+      })
+      it('should display icon', () => {
+        cy.mount(<IconButton icon='menu' to=''/>)
+        cy.get('button i').should('be.visible')
       })
     })
 
