29ff61ae44c9e19642ec6fda53da36f1821ca1ec
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-12-18 17:59:34 +0100

    Rename ENV variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,11 +17,11 @@ import { ItemsQueriesModule } from './queries.module/queries.module';
         return {
           type: 'mongodb',
           authSource: 'admin',
-          host: configService.get('DB_HOST'),
-          port: +configService.get('DB_PORT'),
-          database: configService.get('DB_NAME'),
-          username: configService.get('DB_USER'),
-          password: configService.get('DB_PW'),
+          host: configService.get('DB_MONGO_HOST'),
+          port: +configService.get('DB_MONGO_PORT'),
+          username: configService.get('DB_MONGO_USER'),
+          password: configService.get('DB_MONGO_PW'),
+          database: configService.get('DB_MONGO_NAME'),
           // TODO: Remove this option in production:
           // Only enable this option if your application is in development,
           // otherwise use TypeORM migrations to sync entity schemas:
