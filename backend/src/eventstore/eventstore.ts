9b45fb908c3bdd3adc12ccb8bb20622d67d22eac
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-01-09 13:00:51 +0100

    poc(kafka): add kafka

--- backend/src/eventstore/eventstore.ts
+++ backend/src/eventstore/eventstore.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { Consumer, Kafka, Producer } from 'kafkajs';
 import { BehaviorSubject } from 'rxjs';
 import { ItemCreatedEvent } from '../commands.module/events/item-created.event';
 
@@ -17,25 +18,37 @@ export class EventStore {
 
   private readonly eventMap = new Map([['ItemCreatedEvent', ItemCreatedEvent]]);
 
+  private kafkaConnection: Kafka;
+
+  private kafkaProducer: Producer;
+  private kafkaConsumer: Consumer;
+
   constructor() {
-    this.eventsStream.subscribe((entry) => {
-      if (!entry) return;
-      entry.event.apply(new (this.eventMap.get(entry.type))(entry.event));
-      entry.event.commit();
-      this.logger.log(
-        `Handled #${entry.id} ${entry.type}: ${logStringify(entry.event)}`,
-      );
+    this.kafkaConnection = new Kafka({
+      clientId: 'wwweights-backend',
+      brokers: ['localhost:3007'],
+    });
+    this.kafkaConsumer = this.kafkaConnection.consumer({ groupId: 'backend' });
+    this.kafkaProducer = this.kafkaConnection.producer();
+    this.kafkaProducer.connect();
+
+    this.kafkaConsumer.subscribe({ topics: ['*'] });
+    this.kafkaConsumer.run({
+      eachMessage: async (payload) => console.log(payload),
     });
   }
 
   public addEvent(type: string, event: any) {
-    const eventEntry = { id: this.latestId++, type, event };
-    this.logger.log(
-      `Added #${eventEntry.id} ${eventEntry.type}: ${logStringify(
-        eventEntry.event,
-      )}`,
-    );
-    this.eventsStream.next(eventEntry);
-    return eventEntry.id;
+    this.kafkaProducer.send({
+      topic: type,
+      messages: [
+        {
+          key: 'event',
+          value: event,
+        },
+      ],
+    });
+    this.logger.log('Added event');
   }
+
 }
