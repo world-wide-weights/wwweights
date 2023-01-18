3eb3b8b5bca1b0ed1374634acccb64d5efcbc4fb
Marvin <87226274+mlp909@users.noreply.github.com>
2023-01-18 17:19:23 +0100

    add more iconButton Tests (disabled, active, hover)

--- frontend/src/components/Button/iconButton.cy.tsx
+++ frontend/src/components/Button/iconButton.cy.tsx
@@ -1,32 +1,95 @@
-import '../../styles/global.css';
-import 'material-symbols';
+import "material-symbols";
+import "../../styles/global.css";
 import { IconButton } from "./IconButton";
 
-describe('IconButton', () => {
-    describe('Button with onClick', () => {
-        it('should display component', () => {
-          cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
-          cy.get('button')
-        })
-        it('should display icon', () => {
-          cy.mount(<IconButton icon='menu' onClick={() => ""}/>)
-          cy.get('button i').should('be.visible')
-        })
+describe("IconButton", () => {
+  describe("Button with onClick", () => {
+    beforeEach(() => {
+      cy.mount(<IconButton icon="menu" onClick={() => ""} />);
+    });
+    it("should display component", () => {
+      cy.get("button");
+    });
+    it("should display icon", () => {
+      cy.get("button i").should("be.visible");
+    });
+
+    describe("Button should have hover effect", () => {
+      beforeEach(() => {
+        cy.mount(<IconButton icon="menu" onClick={() => ""} />);
+        cy.get("button").trigger('mouseover')
+      });
+      it("should have hover", () => {
+        cy.get('button').should('have.class', 'cursor-pointer hover:bg-gray-200 focus:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center')
       })
+    });
 
-    describe('Button as link', () => {
-      it('should display component', () => {
-        cy.mount(<IconButton icon='menu' to='/weights'/>)
-        cy.get("a").should("be.visible")
+    describe("Button should be disabled", () => {
+      beforeEach(() => {
+        cy.mount(<IconButton icon="menu" disabled onClick={() => ""} />);
+      });
+      it("should be disabled", () => {
+        cy.get('button').should('have.class', 'cursor-default')
+        cy.get("button").should("be.disabled")
       })
-      it('should display icon', () => {
-        cy.mount(<IconButton icon='menu' to='/weights'/>)
-        cy.get('a i').should('be.visible')
+      it("icon should have opacity", () => {
+        cy.get('button i').should('have.class', 'text-opacity-50')
       })
-    
-    })
+    });
 
-})
+    describe("Button should be active", () => {
+      beforeEach(() => {
+        cy.mount(<IconButton icon="menu" active onClick={() => ""} />);
+      });
+      it("should be active", () => {
+        cy.get('button').should('have.class', 'cursor-default')
+      })
+    });
+  });
 
+  describe("Button as link", () => {
+    beforeEach(() => {
+      cy.mount(<IconButton icon="menu" to="/weights" />);
+    });
+    it("should display component", () => {
+      cy.get("a").should("be.visible");
+    });
+    it("should display icon", () => {
+      cy.get("a i").should("be.visible");
+    });
 
-export { };
+    describe("Link should have hover effect", () => {
+      beforeEach(() => {
+        cy.mount(<IconButton icon="menu" to="/weights" />);
+        cy.get("a").trigger('mouseover')
+      });
+      it("should have hover", () => {
+        cy.get('a').should('have.class', 'cursor-pointer hover:bg-gray-200 focus:bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center')
+      })
+    });
+
+    describe("Link should be disabled", () => {
+      beforeEach(() => {
+        cy.mount(<IconButton icon="menu" disabled to="/weights" />);
+      });
+      it("should be disabled", () => {
+        cy.get('a').should('have.class', 'cursor-default')
+      })
+      it("icon should have opacity", () => {
+        cy.get('a i').should('have.class', 'text-opacity-50')
+      })
+    });
+
+    describe("Link should be active", () => {
+      beforeEach(() => {
+        cy.mount(<IconButton icon="menu" active to="/weights" />);
+      });
+      it("should be active", () => {
+        cy.get('a').should('have.class', 'cursor-default')
+      })
+    });
+  });
+
+});
+
+export {};
