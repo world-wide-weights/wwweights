2016be860202b2893e7d80fee1e1153dd923092d
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-01-19 13:43:00 +0100

    feat(profile): adapt to feedback

--- auth/src/profile/profile.service.ts
+++ auth/src/profile/profile.service.ts
@@ -1,18 +1,35 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { UserService } from '../db/db.service';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserService } from '../db/ user.service';
+import { UserEntity } from '../db/entities/users.entity';
 
 @Injectable()
 export class ProfileService {
+  private readonly logger = new Logger(ProfileService.name);
+
   constructor(private readonly userService: UserService) {}
 
   /**
    * @description Find user and return profile, fail if User does not exist
    */
-  async findUserByIdOrFail(id: number) {
-    const user = await this.userService.findOneById(id);
-    if (!user) {
-      throw new NotFoundException('User was not found');
+  async findUserByIdOrFail(id: number): Promise<UserEntity> {
+    try {
+      const user = await this.userService.findOneById(id);
+      if (!user) {
+        throw new NotFoundException('User was not found');
+      }
+      return user;
+    } catch (error) {
+      this.logger.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Action could not be performed');
     }
-    return user;
   }
 }
