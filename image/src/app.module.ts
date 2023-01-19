0f22c5e731873f9d381248d7ff876b9d19b72d63
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-01-19 14:31:31 +0100

    feat(image upload): init module

--- image/src/app.module.ts
+++ image/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UploadModule],
 })
 export class AppModule {}
---
+++ image/src/upload/upload.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('upload')
+export class UploadController {}
