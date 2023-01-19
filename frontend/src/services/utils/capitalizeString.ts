a81650177c0913ee797e71949328f5c4566dd237
Lars Flieger <a.purenetwork@gmail.com>
2023-01-19 14:56:48 +0100

    Add missing return type

--- frontend/src/services/utils/capitalizeString.ts
+++ frontend/src/services/utils/capitalizeString.ts
@@ -3,6 +3,6 @@
  * @param string we want to have first letter caps.
  * @returns the capitalized string.
  */
-export function capitalizeFirstLetter(string: string) {
+export function capitalizeFirstLetter(string: string): string {
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
