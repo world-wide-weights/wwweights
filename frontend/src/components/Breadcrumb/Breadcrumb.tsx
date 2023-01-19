9c0a855673faf320c3c0eb57526932c323ccc8da
Lars Flieger <a.purenetwork@gmail.com>
2023-01-19 14:43:55 +0100

    Update typo of naming

--- frontend/src/components/Breadcrumb/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumb/Breadcrumb.tsx
@@ -17,7 +17,7 @@ export const Breadcrumb: React.FC<BreadcrumbProps> = ({ breadcrumbs }) => {
 
     return <div datacy="breadcrumb" className="flex items-center gap-2 mb-2">
         <IconButton datacy="breadcrumb-home" icon="home" to={routes.home} />
-        {breadcrumbs.map(({ to, text }, id) => <Fragment key={id}>
+        {breadcrumbs.map(({ to, text }, index) => <Fragment key={index}>
             <Icon className="text-gray-600">chevron_right</Icon>
             <Crumb text={text} to={to} />
         </Fragment>)}
