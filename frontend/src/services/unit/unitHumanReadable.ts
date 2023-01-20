0b52d483d1e43f417aaf03ea832c8389e6d66002
Marvin <87226274+mlp909@users.noreply.github.com>
2023-01-20 23:39:58 +0100

    implemented getBestHumanReadableUnit function with tests

--- frontend/src/services/unit/unitHumanReadable.ts
+++ frontend/src/services/unit/unitHumanReadable.ts
@@ -1,26 +1,31 @@
-import { Weight } from "../../pages/weights";
-import { Unit } from "../../types/unit";
-import { convertWeightIntoUnit } from "./unitConverter";
+import { Weight } from "../../pages/weights"
+import { Unit } from "../../types/unit"
+import { convertWeightIntoUnit } from "./unitConverter"
 
+/**
+ * Gets value in gram and converts it into best readable Unit 1000g = 1kg.
+ * @param weight the weight to convert.
+ * @returns object with number and new readable unit.
+ */
 export const getBestHumanReadableUnit = (
   weight: Weight
 ): { value: number; unit: Unit } => {
-  const weightValueInGram = weight.value;
-  if (weightValueInGram >= 1e14)
-    return convertWeightIntoUnit(weightValueInGram, "g", "Pg");
-  if (weightValueInGram >= 1e11)
-    return convertWeightIntoUnit(weightValueInGram, "g", "Tg");
-  if (weightValueInGram >= 1e5)
-    return convertWeightIntoUnit(weightValueInGram, "g", "Mg");
-  if (weightValueInGram >= 1e2)
-    return convertWeightIntoUnit(weightValueInGram, "g", "kg");
+  const weightValueInGram = weight.value
+  if (weightValueInGram >= 1e15)
+    return convertWeightIntoUnit(weightValueInGram, "g", "Pg")
+  if (weightValueInGram >= 1e12)
+    return convertWeightIntoUnit(weightValueInGram, "g", "Tg")
+  if (weightValueInGram >= 1e6)
+    return convertWeightIntoUnit(weightValueInGram, "g", "Mg")
+  if (weightValueInGram >= 1e3)
+    return convertWeightIntoUnit(weightValueInGram, "g", "kg")
   if (weightValueInGram >= 1)
-    return convertWeightIntoUnit(weightValueInGram, "g", "g");
-  if (weightValueInGram >= 1e-2)
-    return convertWeightIntoUnit(weightValueInGram, "g", "mg");
-  if (weightValueInGram >= 1e-5)
-    return convertWeightIntoUnit(weightValueInGram, "g", "µg");
-  if (weightValueInGram >= 1e-8)
-    return convertWeightIntoUnit(weightValueInGram, "g", "ng");
-  return convertWeightIntoUnit(weightValueInGram, "g", "pg");
-};
+    return convertWeightIntoUnit(weightValueInGram, "g", "g")
+  if (weightValueInGram >= 1e-3)
+    return convertWeightIntoUnit(weightValueInGram, "g", "mg")
+  if (weightValueInGram >= 1e-6)
+    return convertWeightIntoUnit(weightValueInGram, "g", "µg")
+  if (weightValueInGram >= 1e-9)
+    return convertWeightIntoUnit(weightValueInGram, "g", "ng")
+  return convertWeightIntoUnit(weightValueInGram, "g", "pg")
+}
--- frontend/src/services/unit/unitHumanReadable.unit.ts
+++ frontend/src/services/unit/unitHumanReadable.unit.ts
@@ -1,40 +1,123 @@
 import { getBestHumanReadableUnit } from "./unitHumanReadable"
-import { Weight } from "../../pages/weights";
-
-const weight: Weight = {
-    value: 1,
-    additionalValue: 0,
-    isCa: false
-}
 
 
 describe("unitHumanReadable", () => {
-    it("test", () => {
+    it("it should choose Pg for best readable Unit", () => {
         expect(getBestHumanReadableUnit({
-            value: 100000000000,
+            value: 1000000000000000,
             additionalValue: 0,
             isCa: false
-        })).deep.equal({ value: 1, unit: "g" })
+        })).deep.equal({ value: 1, unit: "Pg" })
+        expect(getBestHumanReadableUnit({
+            value: 15300000000000000,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 15.3, unit: "Pg" })
+        expect(getBestHumanReadableUnit({
+            value: 77700000000000000000,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 77700, unit: "Pg" })
     })
-    it("test", () => {
+    it("it should choose Tg for best readable Unit", () => {
         expect(getBestHumanReadableUnit({
-            value: 145454545,
+            value: 1000000000000,
             additionalValue: 0,
             isCa: false
-        })).deep.equal({ value: 1, unit: "g" })
+        })).deep.equal({ value: 1, unit: "Tg" })
+        expect(getBestHumanReadableUnit({
+            value: 15300000000000,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 15.3, unit: "Tg" })
     })
-    it("test", () => {
+    it("it should choose Mg for best readable Unit", () => {
         expect(getBestHumanReadableUnit({
-            value: 0.378379,
+            value: 145000000,
             additionalValue: 0,
             isCa: false
-        })).deep.equal({ value: 1, unit: "g" })
+        })).deep.equal({ value: 145, unit: "Mg" })
+        expect(getBestHumanReadableUnit({
+            value: 8240000000,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 8240, unit: "Mg" })
     })
-    it("test", () => {
+    it("it should choose kg for best readable Unit", () => {
         expect(getBestHumanReadableUnit({
-            value: 0.332,
+            value: 1000,
             additionalValue: 0,
             isCa: false
-        })).deep.equal({ value: 1, unit: "g" })
+        })).deep.equal({ value: 1, unit: "kg" })
+        expect(getBestHumanReadableUnit({
+            value: 160000,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 160, unit: "kg" })
+    })
+    it("it should choose g for best readable Unit", () => {
+        expect(getBestHumanReadableUnit({
+            value: 400,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 400, unit: "g" })
+        expect(getBestHumanReadableUnit({
+            value: 3,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 3, unit: "g" })
+    })
+    it("it should choose mg for best readable Unit", () => {
+        expect(getBestHumanReadableUnit({
+            value: 0.345,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 345, unit: "mg" })
+        expect(getBestHumanReadableUnit({
+            value: 0.034,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 34, unit: "mg" })
+    })
+    it("it should choose µg for best readable Unit", () => {
+        expect(getBestHumanReadableUnit({
+            value: 0.000732,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 732, unit: "µg" })
+        expect(getBestHumanReadableUnit({
+            value: 0.000099,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 99, unit: "µg" })
+    })
+    it("it should choose ng for best readable Unit", () => {
+        expect(getBestHumanReadableUnit({
+            value: 0.000000345,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 345, unit: "ng" })
+        expect(getBestHumanReadableUnit({
+            value: 0.000000640,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 640, unit: "ng" })
+    })
+    it("it should choose pg for best readable Unit", () => {
+        expect(getBestHumanReadableUnit({
+            value: 0.000000000777,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 777, unit: "pg" })
+        expect(getBestHumanReadableUnit({
+            value: 0.000000000009,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 9, unit: "pg" })
+        expect(getBestHumanReadableUnit({
+            value: 0.000000000000004,
+            additionalValue: 0,
+            isCa: false
+        })).deep.equal({ value: 0.004, unit: "pg" })
     })
 })
