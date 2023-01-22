7c3c36e269c8eb9d8f7f6cb557e5fdf76f7b3bb1
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-01-22 16:21:21 +0100

    chore(docker compose): update auth backend ports

--- backend/auth/src/main.ts
+++ backend/auth/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3005);
+  await app.listen(3001);
 }
 bootstrap();
