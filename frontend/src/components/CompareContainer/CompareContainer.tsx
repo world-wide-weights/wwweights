165bda0683e9c6988c283f46a0b42e1e504d7a27
Lars Flieger <a.purenetwork@gmail.com>
2023-01-22 17:42:38 +0100

    Add comments

--- frontend/src/components/CompareContainer/CompareContainer.tsx
+++ frontend/src/components/CompareContainer/CompareContainer.tsx
@@ -2,10 +2,15 @@ import BigNumber from "bignumber.js"
 import { CompareCard, CompareTypes, compareTypes } from "../Statistics/CompareCard"
 
 type CompareContainerProps = {
+    /** Specify weight in gram of item. */
     weight: number
+    /** Specify name of item. */
     itemName: string
 }
-// TODO: Ref naming here
+
+/**
+ * Displays CompareCards based on given weight.
+ */
 export const CompareContainer: React.FC<CompareContainerProps> = ({ weight, itemName }) => {
     return <div className="lg:w-1/2">
         {Object.entries(compareTypes).map(([type, compareType]) => {
