666212c18b917c93841b19ab98d3172178e73189
Marvin <87226274+mlp909@users.noreply.github.com>
2023-01-22 20:33:09 +0100

    add opacity, color test

--- frontend/src/components/Chip/chip.cy.tsx
+++ frontend/src/components/Chip/chip.cy.tsx
@@ -2,15 +2,26 @@ import "../../styles/global.css"
 import { Chip } from "./Chip"
 
 describe("Chip", () => {
-    it("should display correct color", () => {
-        const color = "amber"
+  it("should display default color", () => {
 
-        cy.mount(<Chip to="/" color={"amber"}>Test</Chip>)
+    cy.mount(<Chip to="/">Test</Chip>)
 
-        cy.get("a").should("have.class", `bg-${color}-500`)
-        cy.get("a").should("have.class", `text-${color}-600`)
-    })
+    cy.get("a").should("have.class", "bg-blue-500")
+    cy.get("a").should("have.class", "text-blue-600")
+  })
+  
+  it("should display correct color", () => {
+    const color = "amber"
+
+    cy.mount(<Chip to="/" color={"amber"}>Test</Chip>)
+
+    cy.get("a").should("have.class", `bg-${color}-500`)
+    cy.get("a").should("have.class", `text-${color}-600`)
+  })
+
+  it("should have opacity if disabled", () => {
+    cy.mount(<Chip to="/" disabled>Test</Chip>)
+
+    cy.get("a").should("have.class", "text-opacity-60")
+  })
 })
-
-export { }
-
