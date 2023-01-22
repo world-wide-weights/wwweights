25b59ebeb8605cd573a5839388b7b890768ccf5c
Marvin <87226274+mlp909@users.noreply.github.com>
2023-01-22 20:39:12 +0100

    more tests for chip as button

--- frontend/src/components/Chip/chip.cy.tsx
+++ frontend/src/components/Chip/chip.cy.tsx
@@ -2,26 +2,65 @@ import "../../styles/global.css"
 import { Chip } from "./Chip"
 
 describe("Chip", () => {
-  it("should display default color", () => {
+  describe("Chip as Link", () => {
+    it("chip as link should be visible", () => {
 
-    cy.mount(<Chip to="/">Test</Chip>)
-
-    cy.get("a").should("have.class", "bg-blue-500")
-    cy.get("a").should("have.class", "text-blue-600")
-  })
+      cy.mount(<Chip to="/">Test</Chip>)
   
-  it("should display correct color", () => {
-    const color = "amber"
+      cy.get("a").should("be.visible")
+    })
+    it("should display default color", () => {
 
-    cy.mount(<Chip to="/" color={"amber"}>Test</Chip>)
+      cy.mount(<Chip to="/">Test</Chip>)
+  
+      cy.get("a").should("have.class", "bg-blue-500")
+      cy.get("a").should("have.class", "text-blue-600")
+    })
+  
+    it("should display correct color", () => {
+      const color = "amber"
+  
+      cy.mount(<Chip to="/" color={"amber"}>Test</Chip>)
+  
+      cy.get("a").should("have.class", `bg-${color}-500`)
+      cy.get("a").should("have.class", `text-${color}-600`)
+    })
 
-    cy.get("a").should("have.class", `bg-${color}-500`)
-    cy.get("a").should("have.class", `text-${color}-600`)
+    it("should have opacity if disabled", () => {
+      cy.mount(<Chip to="/" disabled>Test</Chip>)
+  
+      cy.get("a").should("have.class", "text-opacity-60")
+    })
   })
 
-  it("should have opacity if disabled", () => {
-    cy.mount(<Chip to="/" disabled>Test</Chip>)
+  describe("Chip as Button", () => {
+    it("chip as button should be visible", () => {
 
-    cy.get("a").should("have.class", "text-opacity-60")
+      cy.mount(<Chip onClick={() => ""}>Test</Chip>)
+  
+      cy.get("button").should("be.visible")
+    })
+    it("should display default color", () => {
+
+      cy.mount(<Chip onClick={() => ""}>Test</Chip>)
+  
+      cy.get("button").should("have.class", "bg-blue-500")
+      cy.get("button").should("have.class", "text-blue-600")
+    })
+  
+    it("should display correct color", () => {
+      const color = "amber"
+  
+      cy.mount(<Chip onClick={() => ""} color={"amber"}>Test</Chip>)
+  
+      cy.get("button").should("have.class", `bg-${color}-500`)
+      cy.get("button").should("have.class", `text-${color}-600`)
+    })
+
+    it("should have opacity if disabled", () => {
+      cy.mount(<Chip onClick={() => ""} disabled>Test</Chip>)
+  
+      cy.get("button").should("have.class", "text-opacity-60")
+    })
   })
 })
