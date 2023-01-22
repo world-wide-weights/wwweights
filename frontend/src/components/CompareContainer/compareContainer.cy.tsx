5831773421c9e86ec254a0d8ccca13b55c4d6d9c
Lars Flieger <a.purenetwork@gmail.com>
2023-01-22 17:41:08 +0100

    Update tests to remove dep from code

--- frontend/src/components/CompareContainer/compareContainer.cy.tsx
+++ frontend/src/components/CompareContainer/compareContainer.cy.tsx
@@ -1,32 +1,37 @@
 import "material-symbols"
 import "../../styles/global.css"
-import { compareTypes } from "../Statistics/CompareCard"
 import { CompareContainer } from "./CompareContainer"
+
+const WATER_BOTTLE_WEIGHT = 1_100
+const PERSON_WEIGHT = 62_000
+const EARTH_WEIGHT = 5.97200000000000062e+27
+const AIRPLANE_WEIGHT = 1e+8
+const CAR_WEIGHT = 1_400_000
 
 describe("Stats Compare Card", () => {
     it("should display water bottles", () => {
-        cy.mount(<CompareContainer weight={compareTypes["waterBottle"].weight + 1} itemName="Test" />)
-        cy.contains(compareTypes["waterBottle"].singular).should("be.visible")
+        cy.mount(<CompareContainer weight={WATER_BOTTLE_WEIGHT + 1} itemName="Test" />)
+        cy.contains("Water bottle").should("be.visible")
     })
 
     it("should display carTruck only when weight is greater than a car", () => {
-        cy.mount(<CompareContainer weight={compareTypes["carTruck"].weight + 1} itemName="Test" />)
-        cy.contains(compareTypes["carTruck"].singular).should("be.visible")
+        cy.mount(<CompareContainer weight={CAR_WEIGHT + 1} itemName="Test" />)
+        cy.contains("Car").should("be.visible")
     })
 
     it("should display earths only when weight is greater or equal than earth", () => {
-        cy.mount(<CompareContainer weight={compareTypes["earths"].weight + 1} itemName="Test" />)
-        cy.contains(compareTypes["earths"].singular).should("be.visible")
+        cy.mount(<CompareContainer weight={EARTH_WEIGHT + 1} itemName="Test" />)
+        cy.contains("Earth").should("be.visible")
     })
 
     it("should display people only when weight is greater than a person", () => {
-        cy.mount(<CompareContainer weight={compareTypes["people"].weight + 1} itemName="Test" />)
-        cy.contains(compareTypes["people"].singular).should("be.visible")
+        cy.mount(<CompareContainer weight={PERSON_WEIGHT + 1} itemName="Test" />)
+        cy.contains("Person").should("be.visible")
     })
 
     it("should display airplaneTitanic only when weight is greater than an airplane", () => {
-        cy.mount(<CompareContainer weight={compareTypes["airplaneTitanic"].weight + 1} itemName="Test" />)
-        cy.contains(compareTypes["airplaneTitanic"].singular).should("be.visible")
+        cy.mount(<CompareContainer weight={AIRPLANE_WEIGHT + 1} itemName="Test" />)
+        cy.contains("Airplane").should("be.visible")
     })
 })
 
