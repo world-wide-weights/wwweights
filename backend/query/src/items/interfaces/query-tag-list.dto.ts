68da8179171b963a85dc29c50a9e59c563cec6a9
Lars Flieger <a.purenetwork@gmail.com>
2023-01-23 01:59:30 +0100

    Update QueryTagListDto

--- backend/query/src/items/interfaces/query-tag-list.dto.ts
+++ backend/query/src/items/interfaces/query-tag-list.dto.ts
@@ -1,11 +1,10 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 import { Page } from './page';
-import { SortEnum } from './sortEnum';
 
 export class QueryTagListDto extends Page {
   @IsString()
   @IsOptional()
   @ApiPropertyOptional()
-  sort = SortEnum.RELEVANCE; // asc, desc, most-used, relevance // Not an ENUM because ENUMS are inperformant and can lead to spaghetti code
+  sort = 'ASC'; // asc, desc, most-used, relevance // Not an ENUM because ENUMS are inperformant and can lead to spaghetti code
 }
