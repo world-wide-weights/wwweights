e663e63db293a5dc51c0f64bcfb18af8f43e7b81
Lars Flieger <a.purenetwork@gmail.com>
2023-02-03 17:47:32 +0100

    Fix bug with array of titles

--- frontend/src/components/Seo/Seo.tsx
+++ frontend/src/components/Seo/Seo.tsx
@@ -41,29 +41,32 @@ export const Seo: React.FC<BaseSeoProps> = ({
     ogImageWidth = "1200px",
     ogImageDescription = `Preview Image of page "${title}"`,
     twitterImage = `${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/img/twitter_default.jpg`
-}) => (<Head>
-    {/** Define per page */}
-    <title key="title">{title}{shouldDisplayTitleSuffix && baseTitleSuffix}</title>
-    <meta name="description" content={description} key="metaDescription" />
-    <meta name="keywords" content={generateKeywordString([...keywords, ...baseKeywords])} key="metaKeywords" />
+}) => {
+    const finalTitle = `${title}${shouldDisplayTitleSuffix && baseTitleSuffix}`
+    return <Head>
+        {/** Define per page */}
+        <title key="title">{finalTitle}</title>
+        <meta name="description" content={description} key="metaDescription" />
+        <meta name="keywords" content={generateKeywordString([...keywords, ...baseKeywords])} key="metaKeywords" />
 
-    {/** Open Graph */}
-    <meta property="og:url" content={`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}${canonicalLink}`} key="ogUrl" />
-    <meta property="og:title" content={`${title}${shouldDisplayTitleSuffix && baseTitleSuffix}`} key="ogTitle" />
-    <meta property="og:site_name " content={siteTitle} key="ogStiteName" />
-    <meta property="og:description" content={description} key="ogDescription" />
+        {/** Open Graph */}
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}${canonicalLink}`} key="ogUrl" />
+        <meta property="og:title" content={finalTitle} key="ogTitle" />
+        <meta property="og:site_name " content={siteTitle} key="ogStiteName" />
+        <meta property="og:description" content={description} key="ogDescription" />
 
-    {ogImage && <>
-        <meta property="og:image" content={ogImage} key="ogImage" />
-        <meta property="og:image:type" content="image/jpeg" key="ogImageType" />
-        <meta property="og:image:width" content={ogImageWidth} key="ogImageWidth" /> {/** min. Recommend 1200x630 */}
-        <meta property="og:image:height" content={ogImageHeight} key="ogImageHeight" />
-        <meta property="og:image:alt" content={ogImageDescription} key="ogImageAlt" />
-    </>}
+        {ogImage && <>
+            <meta property="og:image" content={ogImage} key="ogImage" />
+            <meta property="og:image:type" content="image/jpeg" key="ogImageType" />
+            <meta property="og:image:width" content={ogImageWidth} key="ogImageWidth" /> {/** min. Recommend 1200x630 */}
+            <meta property="og:image:height" content={ogImageHeight} key="ogImageHeight" />
+            <meta property="og:image:alt" content={ogImageDescription} key="ogImageAlt" />
+        </>}
 
-    {/** Twitter */}
-    <meta name="twitter:card" content="summary_large_image" key="twitterCard" />
-    <meta name="twitter:site" content="@wwweights" key="twitterSite" />
-    <meta name="twitter:creator" content="@wwweights" key="twitterCreator" />
-    {twitterImage && <meta name="twitter:image" content={twitterImage} key="twitterImage" />}
-</Head>)
+        {/** Twitter */}
+        <meta name="twitter:card" content="summary_large_image" key="twitterCard" />
+        <meta name="twitter:site" content="@wwweights" key="twitterSite" />
+        <meta name="twitter:creator" content="@wwweights" key="twitterCreator" />
+        {twitterImage && <meta name="twitter:image" content={twitterImage} key="twitterImage" />}
+    </Head>
+}
