35193e28c6915082ee9d67dd004453bbd26410c2
Lars Flieger <a.purenetwork@gmail.com>
2023-02-03 17:07:34 +0100

    Add comment to function

--- frontend/src/services/seo/keywords.ts
+++ frontend/src/services/seo/keywords.ts
@@ -1,4 +1,9 @@
 
+/**
+ * Generates a string of keywords separated by commas.
+ * @param {string[]} keywords An array of keywords.
+ * @returns {string} The generated string, a comma-separated list of keywords.
+ */
 
 export const generateKeywordString = (keywords: string[]): string => {
     return keywords.join(", ")
