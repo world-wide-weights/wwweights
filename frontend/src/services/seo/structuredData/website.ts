6007324fa5fd582491a4e5b9f1434c82b0058ba9
Lars Flieger <a.purenetwork@gmail.com>
2023-02-05 13:57:42 +0100

    Update PR findings "var instead of method"

--- frontend/src/services/seo/structuredData/website.ts
+++ frontend/src/services/seo/structuredData/website.ts
@@ -1,19 +1,17 @@
-export const getStructuredDataWebsite = () => {
-  return {
-    __html: JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "WebSite",
-      "url": `${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}`,
-      "name": "World Wide Weights",
-      "alternateName": "WWWeights",
-      "potentialAction": {
-        "@type": "SearchAction",
-        "target": {
-          "@type": "EntryPoint",
-          "urlTemplate": `${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/weights?query={search_term_string}`
-        },
-        "query-input": "required name=search_term_string"
-      }
-    })
-  }
+export const getStructuredDataWebsite = {
+  __html: JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "url": `${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}`,
+    "name": "World Wide Weights",
+    "alternateName": "WWWeights",
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": {
+        "@type": "EntryPoint",
+        "urlTemplate": `${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/weights?query={search_term_string}`
+      },
+      "query-input": "required name=search_term_string"
+    }
+  })
 }
