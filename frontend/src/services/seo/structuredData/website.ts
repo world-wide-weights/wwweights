441652e6ac244d5cfae5ac37b9dc888d46991c25
Lars Flieger <a.purenetwork@gmail.com>
2023-02-05 14:00:46 +0100

    Update PR findings "Add comment to getStructuredDataWebsite"

--- frontend/src/services/seo/structuredData/website.ts
+++ frontend/src/services/seo/structuredData/website.ts
@@ -1,3 +1,11 @@
+/**
+ * Creates structured data for the website in JSON-LD format.
+ * The data includes information about the website URL, name, alternate name, 
+ * and potential action for search.
+ * 
+ * The structured data follows the schema.org WebSite specification, 
+ * and is used to improve search engine optimization (SEO) and give context to search engines.
+ */
 export const getStructuredDataWebsite = {
   __html: JSON.stringify({
     "@context": "https://schema.org",
