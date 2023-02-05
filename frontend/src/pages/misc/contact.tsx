929d9674249b6707c6d2bb547707aa424cde88fe
Lars Flieger <a.purenetwork@gmail.com>
2023-02-05 15:04:54 +0100

    Update contact page

--- frontend/src/pages/misc/contact.tsx
+++ frontend/src/pages/misc/contact.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 import email from "../../../public/email.png"
+import discord from "../../assets/img/logos/discord-logo-blue.svg"
+import github from "../../assets/img/logos/github-logo.png"
 import { PageLayout } from "../../components/Layout/PageLayout"
 import { NextPageCustomProps } from "../_app"
 
@@ -8,8 +10,16 @@ import { NextPageCustomProps } from "../_app"
  */
 const Contact: NextPageCustomProps = () => {
     return <>
-        <p className="mb-2">You are more than welcome to share feedback and request with us. If you need to contact us, please use the following mail:</p>
-        <Image alt="Image of E-Mail Adresse" src={email} />
+        <Image className="mb-4" alt="Image of E-Mail Adresse" src={email} />
+        <p className="mb-4">You are more than welcome to share feedback and request with us.</p>
+        <div className="flex">
+            <a href="https://discord.gg/UmxWf2FEQx" className="mb-2 mr-2" target="_blank" title="Link to our discord server!" rel="noreferrer">
+                <Image height={40} alt="Image of Discord Logo" src={discord} />
+            </a>
+            <a href="https://github.com/world-wide-weights" className="mb-2" target="_blank" title="Link to our GitHub Orga!" rel="noreferrer">
+                <Image height={40} alt="Image of GitHub Logo" src={github} />
+            </a>
+        </div>
     </>
 }
 
