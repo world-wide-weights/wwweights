ca8802fe4f4ce58b88b947cfdcb07f12e9d3ef63
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-13 18:00:26 +0100

    Delete itemsByTags

--- backend/command/test/items.e2e-spec.ts
+++ backend/command/test/items.e2e-spec.ts
@@ -11,7 +11,6 @@ import { ItemCronJobHandler } from '../src/items/cron/items.cron';
 import { ItemsModule } from '../src/items/items.module';
 import { EditSuggestion } from '../src/models/edit-suggestion.model';
 import { Item } from '../src/models/item.model';
-import { ItemsByTag } from '../src/models/items-by-tag.model';
 import { Profile } from '../src/models/profile.model';
 import { Tag } from '../src/models/tag.model';
 import { ENVGuard } from '../src/shared/guards/env.guard';
@@ -40,7 +39,6 @@ describe('ItemsController (e2e)', () => {
   let app: INestApplication;
   let itemModel: Model<Item>;
   let tagModel: Model<Tag>;
-  let itemsByTagModel: Model<ItemsByTag>;
   let editSuggestionModel: Model<EditSuggestion>;
   let profileModel: Model<Profile>;
   const mockEventStore: MockEventStore = new MockEventStore();
@@ -79,7 +77,6 @@ describe('ItemsController (e2e)', () => {
 
     itemModel = moduleFixture.get('ItemModel');
     tagModel = moduleFixture.get('TagModel');
-    itemsByTagModel = moduleFixture.get('ItemsByTagModel');
     editSuggestionModel = moduleFixture.get('EditSuggestionModel');
     profileModel = moduleFixture.get('ProfileModel');
 
@@ -99,7 +96,6 @@ describe('ItemsController (e2e)', () => {
     mockEventStore.reset();
     await itemModel.deleteMany();
     await tagModel.deleteMany();
-    await itemsByTagModel.deleteMany();
     await editSuggestionModel.deleteMany();
     await profileModel.deleteMany();
   });
@@ -127,15 +123,7 @@ describe('ItemsController (e2e)', () => {
         .send(insertItem)
         .expect(HttpStatus.OK);
 
-      // We have to wait because of the async nature of the command
-      // const attemptSuccess = await retries([
-      //   { model: itemModel, count: 1 },
-      //   { model: tagModel, count: 2 },
-      //   { model: itemsByTagModel, count: 2 },
-      // ]);
-      // expect(attemptSuccess).toBeTruthy();
-
-      // Also give it some extra time to make sure the tag updates have been completed
+      // Give it some extra time to make sure the tag updates have been completed
       await timeout(500);
 
       const item = await itemModel.findOne({});
@@ -150,18 +138,6 @@ describe('ItemsController (e2e)', () => {
       expect(tag1.count).toEqual(1);
       expect(tag2.count).toEqual(1);
 
-      const itemsByTag1 = await itemsByTagModel.findOne({ tagName: 'tag1' });
-      const itemsByTag2 = await itemsByTagModel.findOne({ tagName: 'tag2' });
-
-      expect(itemsByTag1).toBeDefined();
-      expect(itemsByTag1.items.length).toEqual(1);
-      expect(itemsByTag1.items[0].tags[0].name).toEqual(tag1.name);
-      expect(itemsByTag1.items[0].tags[1].name).toEqual(tag2.name);
-
-      expect(itemsByTag2).toBeDefined();
-      expect(itemsByTag2.items.length).toEqual(1);
-      expect(itemsByTag2.items[0].tags[0].name).toEqual(tag1.name);
-      expect(itemsByTag2.items[0].tags[1].name).toEqual(tag2.name);
       return;
     });
 
@@ -191,19 +167,6 @@ describe('ItemsController (e2e)', () => {
       expect(tag1.count).toEqual(2);
       expect(tag2.count).toEqual(1);
 
-      const itemsByTag1 = await itemsByTagModel.findOne({ tagName: 'tag1' });
-      const itemsByTag2 = await itemsByTagModel.findOne({ tagName: 'tag2' });
-
-      expect(itemsByTag1).toBeDefined();
-      expect(itemsByTag1.items.length).toEqual(2);
-      expect(itemsByTag1.items[0].tags[0].name).toEqual(tag1.name);
-      expect(itemsByTag1.items[0].tags[1].name).toEqual(tag2.name);
-      expect(itemsByTag1.items[1].tags[0].name).toEqual(tag1.name);
-
-      expect(itemsByTag2).toBeDefined();
-      expect(itemsByTag2.items.length).toEqual(1);
-      expect(itemsByTag2.items[0].tags[0].name).toEqual(tag1.name);
-      expect(itemsByTag2.items[0].tags[1].name).toEqual(tag2.name);
       return;
     });
 
@@ -454,44 +417,6 @@ describe('ItemsController (e2e)', () => {
       expect(oldTag.count).toEqual(item.tags[0].count - 1);
     });
 
-    it('items/:slug/suggest/edit => Should update tags ItemsByTag', async () => {
-      // ARRANGE
-      const item = new itemModel(singleItem);
-      await item.save();
-      await tagModel.insertMany(item.tags);
-      await itemsByTagModel.insertMany([
-        { tagName: singleItem.tags[0].name, items: [item] },
-        { tagName: singleItem.tags[1].name, items: [item] },
-      ]);
-      // Create eventstore stream
-      mockEventStore.existingStreams = [
-        `${ALLOWED_EVENT_ENTITIES.ITEM}-${item.slug}`,
-      ];
-      //ACT
-      const res = await request(server)
-        .post(commandsPath + `items/${item.slug}/suggest/edit`)
-        .send({
-          tags: { push: ['newTag1'], pull: [item.tags[0].name] },
-        });
-      // ASSERT
-      expect(res.status).toEqual(HttpStatus.OK);
-      await retryCallback(async () => {
-        // Has new tag been created?
-        return (
-          (await itemsByTagModel.findOne({ tagName: item.tags[0].name })).items
-            .length === 0
-        );
-      });
-      const newTag = await itemsByTagModel.findOne({ tagName: 'newTag1' });
-      const oldTag = await itemsByTagModel.findOne({
-        tagName: item.tags[0].name,
-      });
-      // Has tag been created?
-      expect(newTag).toBeDefined();
-      expect(newTag.items[0].slug).toEqual(item.slug);
-      expect(oldTag.items.length).toEqual(0);
-    });
-
     it('items/insert => increment profile counts', async () => {
       await request(server)
         .post(commandsPath + 'items/insert')
@@ -609,57 +534,6 @@ describe('ItemsController (e2e)', () => {
     });
   });
 
-  describe('correctAllItemsByTagCounts (CRON)', () => {
-    it('Should update ItemsByTag', async () => {
-      const item1 = {
-        ...singleItem,
-        tags: [
-          {
-            ...singleItem.tags[0],
-            count: 2,
-          },
-          {
-            ...singleItem.tags[1],
-          },
-        ],
-      };
-      const item2 = {
-        ...singleItem,
-        name: 'justTest',
-        slug: 'justtest',
-        tags: [{ ...singleItem.tags[0], count: 2 }],
-      };
-      // ARRANGE
-      await itemModel.insertMany([item1, item2]);
-      await tagModel.insertMany(singleItemTags);
-      await itemsByTagModel.insertMany([
-        {
-          tagName: singleItem.tags[0].name,
-          items: [
-            {
-              ...item1,
-              tags: [{ ...item1.tags[0], count: 1 }, item1.tags[1]],
-            },
-            { ...item2, tags: [{ ...item2.tags[0], count: 0 }] },
-          ],
-        },
-        {
-          tagName: item1.tags[1].name,
-          items: [item1],
-        },
-      ]);
-      // ACT
-      await itemCronJobHandler.correctAllItemsByTagCounts();
-      // ASSERT
-      const firstTag = await itemsByTagModel.findOne({
-        tagName: singleItem.tags[0].name,
-      });
-      expect(firstTag.items.length).toEqual(2);
-      expect(firstTag.items[0].tags[0].count).toEqual(2);
-      expect(firstTag.items[1].tags[0].count).toEqual(2);
-    });
-  });
-
   describe('deleteUnusedTags (CRON)', () => {
     it('Should delete unused Tags from Tags', async () => {
       // ARRANGE
@@ -676,57 +550,15 @@ describe('ItemsController (e2e)', () => {
         newTagList.find((e) => e.name === 'tobedeleted' && e.count === 0),
       ).toBeUndefined();
     });
-    it('Should delete unused Tags from ItemsByTag', async () => {
-      // ARRANGE
-      await tagModel.insertMany([
-        ...singleItemTags,
-        { name: 'tobedeleted', count: 0 },
-      ]);
-      await itemsByTagModel.insertMany([
-        {
-          tagName: 'tobedeleted',
-          items: [],
-        },
-        {
-          tagName: singleItemTags[0].name,
-          items: [singleItem],
-        },
-        {
-          tagName: singleItemTags[1].name,
-          items: [singleItem],
-        },
-      ]);
-      // ACT
-      await itemCronJobHandler.deleteUnusedTags();
-      // ASSERT
-      // Should now only contain
-      const updatedItemsByTags = await itemsByTagModel.find({});
-      expect(updatedItemsByTags.length).toEqual(2);
-      expect(
-        updatedItemsByTags.find((e) => e.tagName === 'tobedeleted'),
-      ).toBeUndefined();
-    });
 
     it('Should work even when no changes are needed', async () => {
       // ARRANGE
       await tagModel.insertMany(singleItemTags);
-      await itemsByTagModel.insertMany([
-        {
-          tagName: singleItemTags[0].name,
-          items: [singleItem],
-        },
-        {
-          tagName: singleItemTags[1].name,
-          items: [singleItem],
-        },
-      ]);
       // ACT
       await itemCronJobHandler.deleteUnusedTags();
       // ASSERT
       const tags = await tagModel.find({});
       expect(tags.length).toEqual(singleItemTags.length);
-      const itemsByTags = await itemsByTagModel.find({});
-      expect(itemsByTags.length).toEqual(2);
     });
   });
 });
