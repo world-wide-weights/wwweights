6f637b8171a115eb78251454a98e135753a1abc2
Lars Flieger <a.purenetwork@gmail.com>
2023-02-14 14:06:32 +0100

    Update component tests to use new datacy

--- frontend/src/components/Form/ImageUpload/imageUpload.cy.tsx
+++ frontend/src/components/Form/ImageUpload/imageUpload.cy.tsx
@@ -7,6 +7,8 @@ const initialValues = {
     file: ""
 }
 
+const NAME = "file"
+
 const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return <Formik initialValues={initialValues} onSubmit={(values) => console.log(values)} >
         <Form>
@@ -18,58 +20,58 @@ const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 describe("Image Upload", () => {
     beforeEach(() => {
         cy.mount(<Wrapper>
-            <ImageUpload name="file" />
+            <ImageUpload name={NAME} />
         </Wrapper>)
     })
 
     describe("Basic", () => {
         it("should render", () => {
-            cy.dataCy("imageupload-content").should("be.visible")
+            cy.dataCy(`imageupload-${NAME}-content`).should("be.visible")
         })
 
         it("should select a file and show preview image", () => {
-            cy.dataCy("imageupload-file-input").selectFile({
+            cy.dataCy(`imageupload-${NAME}-file-input`).selectFile({
                 contents: Cypress.Buffer.from("file contents"),
                 fileName: "file.png",
                 lastModified: Date.now(),
             }, { force: true })
 
-            cy.dataCy("imageupload-image").should("be.visible")
+            cy.dataCy(`imageupload-${NAME}-image`).should("be.visible")
         })
 
 
         it("should select a file with drag and drop and show preview image", () => {
-            cy.dataCy("imageupload-file-input").selectFile({
+            cy.dataCy(`imageupload-${NAME}-file-input`).selectFile({
                 contents: Cypress.Buffer.from("file contents"),
                 fileName: "file.png",
                 lastModified: Date.now(),
             }, { force: true, action: "drag-drop" })
 
-            cy.dataCy("imageupload-image").should("be.visible")
+            cy.dataCy(`imageupload-${NAME}-image`).should("be.visible")
         })
 
         it("should remove image when clicking on the remove button", () => {
-            cy.dataCy("imageupload-file-input").selectFile({
+            cy.dataCy(`imageupload-${NAME}-file-input`).selectFile({
                 contents: Cypress.Buffer.from("file contents"),
                 fileName: "file.png",
                 lastModified: Date.now(),
             }, { force: true })
 
-            cy.dataCy("imageupload-reset-image").click()
-            cy.dataCy("imageupload-image").should("not.exist")
+            cy.dataCy(`imageupload-${NAME}-reset-image`).click()
+            cy.dataCy(`imageupload-${NAME}-image`).should("not.exist")
         })
     })
 
     describe("Error", () => {
         it("should not allow a file that is not an image", () => {
-            cy.dataCy("imageupload-file-input").selectFile({
+            cy.dataCy(`imageupload-${NAME}-file-input`).selectFile({
                 contents: Cypress.Buffer.from("file contents"),
                 fileName: "file.xls",
                 lastModified: Date.now(),
             }, { force: true })
 
             // TODO (Zoe-Bot): Implement error test when correct error handling is implemented
-            cy.dataCy("imageupload-image").should("not.exist")
+            cy.dataCy(`imageupload-${NAME}-image`).should("not.exist")
         })
     })
 })
