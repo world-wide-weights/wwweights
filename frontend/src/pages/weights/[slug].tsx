adcbc20013b527a4b45a08742f84bf2265242846
Lars Flieger <a.purenetwork@gmail.com>
2023-02-14 13:42:47 +0100

    Fix bug with wrong image url

--- frontend/src/pages/weights/[slug].tsx
+++ frontend/src/pages/weights/[slug].tsx
@@ -49,7 +49,7 @@ export default function WeightsSingle({ item, relatedItems }: InferGetServerSide
     const sourceName = item.source ? new URL(item.source).hostname.replace("www.", "") : null
 
     // Convert image url 
-    item.image = getImageUrl(item.image)
+    const imageUrl = getImageUrl(item.image)
 
     // Throw error when tab does not exist.
     if (currentTabIndex === -1 && currentTab)
@@ -92,10 +92,10 @@ export default function WeightsSingle({ item, relatedItems }: InferGetServerSide
                     </div>
 
                     {/* Weights Image */}
-                    {item.image && <div className="row-start-1 lg:row-end-3 lg:flex lg:justify-end">
+                    {imageUrl && <div className="row-start-1 lg:row-end-3 lg:flex lg:justify-end">
                         {/* No better way yet: https://github.com/vercel/next.js/discussions/21379 Let's take a look at this when we got problems with it */}
-                        <Image src={item.image} priority className="sm:hidden rounded-xl" alt={item.name} width={120} height={120} />
-                        <Image src={item.image} priority className="hidden sm:block rounded-xl" alt={item.name} width={230} height={230} />
+                        <Image src={imageUrl} priority className="sm:hidden rounded-xl" alt={item.name} width={120} height={120} />
+                        <Image src={imageUrl} priority className="hidden sm:block rounded-xl" alt={item.name} width={230} height={230} />
                     </div>}
                 </div>
                 <hr className="mb-4 md:mb-8" />
