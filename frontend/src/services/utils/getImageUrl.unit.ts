f95dbb54cdb15203e46409c6342f662d5ac5f8d3
Lars Flieger <a.purenetwork@gmail.com>
2023-02-14 13:42:56 +0100

    Add test to ensure localhost works as well.

--- frontend/src/services/utils/getImageUrl.unit.ts
+++ frontend/src/services/utils/getImageUrl.unit.ts
@@ -13,6 +13,13 @@ describe("getImageUrl", () => {
         expect(result).to.equal(`${Cypress.env("PUBLIC_API_BASE_URL_IMAGE")}/serve/${image}`)
     })
 
+    // This issue is in yup and is currently in PR (14.02.2023) https://github.com/jquense/yup/pull/1859
+    it.skip("should return image url when url is localhost", () => {
+        const image = "http://localhost:3000/image.png"
+        const result = getImageUrl(image)
+        expect(result).to.equal(image)
+    })
+
     it("should return undefined when image is undefined", () => {
         const result = getImageUrl(undefined)
         expect(result).to.equal(undefined)
