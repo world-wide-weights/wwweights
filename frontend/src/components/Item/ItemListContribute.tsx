583ff36ff7906c034c4d19f8834c0b95c7c76833
Marvin Pfau <marvin.pfau@stihl.de>
2023-02-15 14:36:40 +0100

    add itemListContribute Tests

--- frontend/src/components/Item/ItemListContribute.tsx
+++ frontend/src/components/Item/ItemListContribute.tsx
@@ -29,20 +29,20 @@ export const ItemListContribute: React.FC<ItemListContributeProps> = ({ name, sl
             <Link className="flex justify-between md:justify-start items-center w-full md:h-12 mx-2 md:mx-4" href={routes.weights.single(slug)}>
                 <div className="md:flex">
                     <Tooltip position="left" content={name}>
-                        <h5 className="font-medium truncate w-40 md:w-64 pr-3">{name}</h5>
+                        <h5 datacy="item-name" className="font-medium truncate w-40 md:w-64 pr-3">{name}</h5>
                     </Tooltip>
-                    <h5 className="text-gray-600 font-medium md:w-32 mr-5">{weightString}</h5>
+                    <h5 datacy="item-weight" className="text-gray-600 font-medium md:w-32 mr-5">{weightString}</h5>
                 </div>
-                {imageUrl && <Image className="object-cover rounded-lg w-12 h-12" alt={`Image of ${name}`} src={imageUrl} width={96} height={96} />}
+                {imageUrl && <Image datacy="item-image" className="object-cover rounded-lg w-12 h-12" alt={`Image of ${name}`} src={imageUrl} width={96} height={96} />}
             </Link>
 
             {/* Actions */}
             <div className="flex items-center mx-0 sm:mx-3">
                 <Tooltip content="Edit">
-                    <IconButton icon="edit" />
+                    <IconButton datacy="edit-action" icon="edit" />
                 </Tooltip>
                 <Tooltip content="Delete">
-                    <IconButton icon="close" />
+                    <IconButton datacy="delete-action" icon="close" />
                 </Tooltip>
             </div>
         </div>
---
+++ frontend/src/components/Item/itemListContribute.cy.tsx
@@ -0,0 +1,43 @@
+import "material-symbols"
+import "../../styles/global.css"
+import { ItemListContribute } from "./ItemListContribute"
+
+describe("ItemListContribute", () => {
+    describe("should display item list contribute correct", () => {
+        beforeEach(() => {
+            cy.mount(<ItemListContribute name="Smartphone" slug="smartphone" weight={{ value: 100, isCa: false }} image="https://via.placeholder.com/96.png" />)
+        })
+
+        it("should display item preview grid correct", () => {
+            cy.dataCy("item-list-contribute").should("be.visible")
+        })
+
+        it("should display item name", () => {
+            cy.dataCy("item-name").should("have.text", "Smartphone")
+        })
+
+        it("should display item weight", () => {
+            cy.dataCy("item-weight").should("have.text", "100 g")
+        })
+
+        it("should display item image", () => {
+            cy.dataCy("item-image").should("be.visible")
+        })
+    })
+
+    describe("should display actions correct", () => {
+        it("should display edit action", () => {
+            cy.mount(<ItemListContribute name="Smartphone" slug="smartphone" weight={{ value: 100, isCa: false }} image="https://via.placeholder.com/96.png" />)
+
+            cy.dataCy("edit-action").should("be.visible")
+        })  
+
+        it("should display delete action", () => {
+            cy.mount(<ItemListContribute name="Smartphone" slug="smartphone" weight={{ value: 100, isCa: false }} image="https://via.placeholder.com/96.png" />)
+
+            cy.dataCy("delete-action").should("be.visible")
+        })
+    })
+
+
+})
