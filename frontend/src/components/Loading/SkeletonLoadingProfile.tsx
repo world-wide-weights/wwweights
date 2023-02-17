3ef682b4ac9a5a4300341955ed486f0fbb19a00a
Lars Flieger <a.purenetwork@gmail.com>
2023-02-17 23:56:30 +0100

    Update profile loading

--- frontend/src/components/Loading/SkeletonLoadingProfile.tsx
+++ frontend/src/components/Loading/SkeletonLoadingProfile.tsx
@@ -8,7 +8,7 @@ export const SkeletonLoadingProfile: React.FC = () => {
             <div className="sm:flex lg:flex-col gap-3 2xl:w-1/4 mb-6 lg:mb-0">
                 {/* Meta infos */}
                 <div className="bg-white flex flex-col justify-center md:justify-start sm:w-1/2 md:w-auto items-center rounded-lg py-6 px-4 mb-3 sm:mb-0">
-                    <div className="bg-slate-100 animate-pulse h-28 w-28 rounded-full mb-2"></div>
+                    <div className="bg-blue-100 animate-pulse h-28 w-28 rounded-full mb-2"></div>
                     <div className="bg-slate-100 animate-pulse h-6 w-32 mb-2"></div>
                     <div className="bg-slate-100 animate-pulse h-5 w-52 mb-2"></div>
                 </div>
@@ -17,7 +17,7 @@ export const SkeletonLoadingProfile: React.FC = () => {
                 <div className="flex flex-col gap-3 flex-grow">
                     {new Array(4).fill(0).map((_, index) =>
                         <div key={index} className="flex items-center bg-white h-20 rounded-lg mb-2">
-                            <div className="bg-slate-100 animate-pulse h-14 w-14 rounded-full ml-7 mr-4"></div>
+                            <div className="bg-blue-100 animate-pulse h-14 w-14 rounded-full ml-7 mr-4"></div>
                             <div>
                                 <div className="bg-slate-100 animate-pulse h-4 w-24 mb-2"></div>
                                 <div className="bg-slate-100 animate-pulse h-5 w-16"></div>
