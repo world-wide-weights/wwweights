63ce5bd682f04f7f8d7102982ef102e47e9a908e
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-18 12:18:07 +0100

    add: tests

--- backend/command/test/items.e2e-spec.ts
+++ backend/command/test/items.e2e-spec.ts
@@ -229,22 +229,6 @@ describe('ItemsController (e2e)', () => {
       expect(tag.count).toEqual(itemsWithDifferentNames.length);
     });
 
-    it('items/insert => increment profile counts', async () => {
-      await request(server)
-        .post(commandsPath + itemInsertPath)
-        .send(insertItem)
-        .expect(HttpStatus.OK);
-
-      await retryCallback(async () => (await profileModel.count()) === 1);
-
-      const profile = await profileModel.findOne({});
-      expect(profile.count.itemsCreated).toEqual(1);
-      expect(profile.count.additionalValueOnCreation).toEqual(0);
-      expect(profile.count.tagsUsedOnCreation).toEqual(2);
-      expect(profile.count.sourceUsedOnCreation).toEqual(0);
-      expect(profile.count.imageAddedOnCreation).toEqual(0);
-    });
-
     it('items/:slug/suggest/edit => Should create a suggestion', async () => {
       // ARRANGE
       const item = new itemModel(singleItem);
@@ -709,6 +693,40 @@ describe('ItemsController (e2e)', () => {
       const globalStatistics = await globalStatisticsModel.findOne();
       expect(globalStatistics.totalSuggestions).toEqual(1);
       expect(globalStatistics.totalItems).toEqual(1);
+    });
+  });
+
+  describe('profileStatistics (POSTS)', () => {
+    it('items/insert => increment profile counts', async () => {
+      await request(server)
+        .post(commandsPath + itemInsertPath)
+        .send(insertItem)
+        .expect(HttpStatus.OK);
+
+      await retryCallback(async () => (await profileModel.count()) === 1);
+
+      const profile = await profileModel.findOne({});
+      expect(profile.count.itemsCreated).toEqual(1);
+      expect(profile.count.additionalValueOnCreation).toEqual(0);
+      expect(profile.count.tagsUsedOnCreation).toEqual(2);
+      expect(profile.count.sourceUsedOnCreation).toEqual(0);
+      expect(profile.count.imageAddedOnCreation).toEqual(0);
+    });
+
+    it('items/insert => increment profile counts and accept no tags', async () => {
+      await request(server)
+        .post(commandsPath + itemInsertPath)
+        .send({ name: insertItem.name, weight: insertItem.weight })
+        .expect(HttpStatus.OK);
+
+      await retryCallback(async () => (await profileModel.count()) === 1);
+
+      const profile = await profileModel.findOne({});
+      expect(profile.count.itemsCreated).toEqual(1);
+      expect(profile.count.additionalValueOnCreation).toEqual(0);
+      expect(profile.count.tagsUsedOnCreation).toEqual(0);
+      expect(profile.count.sourceUsedOnCreation).toEqual(0);
+      expect(profile.count.imageAddedOnCreation).toEqual(0);
     });
   });
 });
