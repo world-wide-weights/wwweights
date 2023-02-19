60ed0ffea2a0af8ca79695bb0a634225b8e31012
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-19 13:20:19 +0100

    update: queries

--- backend/query/src/items/queries/item-statistics.query.ts
+++ backend/query/src/items/queries/item-statistics.query.ts
@@ -1,5 +1,8 @@
 import { QueryItemStatisticsDto } from '../dtos/query-item-statistics.dto';
 
+/**
+ * @description Query to retrieve statistics for a list of items
+ */
 export class ItemStatisticsQuery {
   constructor(public readonly dto: QueryItemStatisticsDto) {}
 }
