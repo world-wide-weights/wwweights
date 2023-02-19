ba2899215fc2b4ebdf45d3edf19e76f1364806d4
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-19 14:08:14 +0100

    update: Models

--- backend/query/src/models/item.model.ts
+++ backend/query/src/models/item.model.ts
@@ -1,12 +1,16 @@
-import { ApiResponseProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiResponseProperty } from '@nestjs/swagger';
 import { index, prop } from '@typegoose/typegoose';
 import { Expose, Type } from 'class-transformer';
+import { Tag } from './tag.model';
 
+/**
+ * @description Values defining a Weight or Range
+ */
 class Weight {
   @Expose()
   @prop({ required: true })
   @ApiResponseProperty({ type: Number, example: 1.234e10 })
-  // This is always in grams and scientific notation example: 1.234e10
+  // This is always in grams
   value: number;
 
   @Expose()
@@ -20,20 +24,9 @@ class Weight {
   additionalValue?: number;
 }
 
-// TODO: since aggregate is gone we can replace this
-// To simplify here is Tag again but without the Aggregate since the Tag in Items does not have to look the same as the Tag alone and a composition type is too much work
-class Tag {
-  @Expose()
-  @prop({ required: true })
-  @ApiResponseProperty({ type: String, example: 'Tag Name' })
-  name: string;
-
-  @Expose()
-  @prop({ required: true, default: -1 }) // so we can increment it with everyone else
-  @ApiResponseProperty({ type: Number, example: 3 })
-  count?: number;
-}
-
+/**
+ * @description The Item model containing a textSearch index on name and tags weighted 1000/1 respectively
+ */
 @index(
   { name: 'text', 'tags.name': 'text' },
   { weights: { name: 1000, tags: 1 }, name: 'ItemTextIndex' },
@@ -41,12 +34,12 @@ class Tag {
 export class Item {
   @Expose()
   @prop({ required: true })
-  @ApiResponseProperty({ type: String, example: 'Item Name' })
+  @ApiResponseProperty({ example: 'Apple' })
   name: string;
 
   @Expose()
   @prop({ required: true, unique: true })
-  @ApiResponseProperty({ type: String, example: 'item-name' })
+  @ApiResponseProperty({ example: 'apple' })
   slug: string;
 
   @Expose()
@@ -59,32 +52,38 @@ export class Item {
   weight: Weight;
 
   @Expose()
-  @prop({ array: true, type: () => [Tag], _id: false })
+  @prop({ array: true, type: () => [Tag], _id: false, excludeIndexes: true })
   @Type(() => Tag)
-  @ApiResponseProperty({
-    type: [Tag],
-    example: [{ name: 'Tag Name', count: 3 }],
+  @ApiProperty({
+    type: Tag,
+    isArray: true,
+    example: [{ name: 'fruit', count: 3 }],
   })
   tags?: Tag[];
 
   @Expose()
   @prop()
-  @ApiResponseProperty({ type: String, example: 'https://link.de/img.png' })
+  @ApiResponseProperty({
+    example:
+      'https://wordpress.shuffle-projekt.de/wp-content/uploads/2021/11/Kuhn_Korbinian-400x400.jpg',
+  })
   image?: string;
 
   @Expose()
   @prop()
-  @ApiResponseProperty({ type: String, example: 'https://link.de/stuff' })
+  @ApiResponseProperty({
+    example: 'https://www.quora.com/What-is-the-average-weight-of-an-Apple',
+  })
   source?: string;
 
   @Expose()
   @prop()
-  @ApiResponseProperty({ type: Number, example: 123 })
+  @ApiResponseProperty({ example: 123 })
   userId: number;
 
   @Expose()
   @prop()
-  @ApiResponseProperty({ type: Number, example: 1234567890000 })
+  @ApiResponseProperty({ example: 1234567890000 })
   createdAt?: number;
 
   constructor(partial: Partial<Item>) {
