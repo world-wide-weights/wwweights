c8c7f109c4c3c6819aff1eae6c621c3251d276e4
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-19 12:55:34 +0100

    update: interfaces

--- backend/query/src/items/interfaces/item-statistics.interface.ts
+++ backend/query/src/items/interfaces/item-statistics.interface.ts
@@ -2,18 +2,22 @@ import { ApiResponseProperty } from '@nestjs/swagger';
 import { Expose, Transform } from 'class-transformer';
 import { Item } from '../../models/item.model';
 
+/**
+ * @description Item statistics return object, holds the heaviest, lightest item and average weight of a list of items
+ */
 export class ItemStatistics {
   @Expose()
   @Transform(({ obj }) => new Item(obj.heaviest))
-  // TODO: Update the example when Item changes
   @ApiResponseProperty({ type: Item })
   heaviest: Item;
+
   @Expose()
   @Transform(({ obj }) => new Item(obj.lightest))
   @ApiResponseProperty({ type: Item })
   lightest: Item;
+
   @Expose()
-  @ApiResponseProperty({ type: Number })
+  @ApiResponseProperty({ type: Number, example: 34560000000 })
   averageWeight: number;
 
   constructor(partial: Partial<ItemStatistics>) {
