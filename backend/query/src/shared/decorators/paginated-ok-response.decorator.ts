a2c14a71fa88dd578156559c8c29e105f4021202
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-19 13:47:38 +0100

    add: decorator for generic paginatedResponse

---
+++ backend/query/src/shared/decorators/paginated-ok-response.decorator.ts
@@ -0,0 +1,26 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { PaginatedResponse } from '../interfaces/paginated-result';
+
+export const ApiOkResponsePaginated = <DataDto extends Type<unknown>>(
+  dataDto: DataDto,
+) =>
+  applyDecorators(
+    ApiExtraModels(PaginatedResponse, dataDto),
+    ApiOkResponse({
+      description: `The paginated result of ${dataDto.name}`,
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(PaginatedResponse) },
+          {
+            properties: {
+              data: {
+                type: 'array',
+                items: { $ref: getSchemaPath(dataDto) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
