f5233a61a39b60e7622295608cd1a6f65952dabe
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-02-19 14:39:06 +0100

    update: queries

--- backend/query/src/profiles/queries/profile-statistics.handler.ts
+++ backend/query/src/profiles/queries/profile-statistics.handler.ts
@@ -22,15 +22,27 @@ export class ProfileStatisticsHandler
 
   async execute({
     dto,
-  }: ProfileStatisticsQuery): Promise<{ count: ProfileCounts } | {}> {
+  }: ProfileStatisticsQuery): Promise<
+    { count: ProfileCounts } | Record<string, never>
+  > {
+    const profileStatisticsQueryStartTime = performance.now();
+
     try {
       const profileCounts = await this.profileModel
         .findOne({ userId: dto.userId }, { _id: 0, count: 1 })
         .lean();
       this.logger.log(`ProfileCounts retrieved for: ${dto.userId}`);
 
+      this.logger.debug(
+        `Finished in ${performance.now() - profileStatisticsQueryStartTime} ms`,
+      );
       return profileCounts || {};
     } catch (error) {
+      this.logger.debug(
+        `Failed after ${
+          performance.now() - profileStatisticsQueryStartTime
+        } ms`,
+      );
       this.logger.error(error);
       if (error instanceof NotFoundException) throw error;
       /* istanbul ignore next */
