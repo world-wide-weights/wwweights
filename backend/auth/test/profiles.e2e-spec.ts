0a272ba0da62ed86783bd96efb437f8ac1740ce5
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-02-19 12:28:29 +0100

    cleanup(auth): fix tests & update packages

--- backend/auth/test/profiles.e2e-spec.ts
+++ backend/auth/test/profiles.e2e-spec.ts
@@ -1,12 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/db/entities/users.entity';
 import * as request from 'supertest';
 import { DataSource } from 'typeorm';
-import { AppModule } from '../src/app.module';
+import { AuthModule } from '../src/auth/auth.module';
 import { AuthService } from '../src/auth/auth.service';
 import { UserService } from '../src/db/services/user.service';
+import { ProfileModule } from '../src/profile/profile.module';
+import { SharedModule } from '../src/shared/shared.module';
+import { MockConfigService } from './helpers/configService.helper';
 import { createUser, deleteUserByAttribute } from './helpers/db.helper';
 import { SAMPLE_USER } from './helpers/sample-data.helper';
 import { setupDataSource } from './helpers/typeOrmSetup';
@@ -20,10 +25,24 @@ describe('ProfilesController (e2e)', () => {
   beforeEach(async () => {
     dataSource = await setupDataSource();
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true, ignoreEnvFile: true }),
+        ProfileModule,
+        TypeOrmModule.forRoot(),
+        AuthModule,
+        SharedModule,
+      ],
+      providers: [
+        {
+          provide: ConfigService,
+          useClass: MockConfigService,
+        },
+      ],
     })
       .overrideProvider(DataSource)
       .useValue(dataSource)
+      .overrideProvider(ConfigService)
+      .useClass(MockConfigService)
       .compile();
 
     app = moduleFixture.createNestApplication();
@@ -45,6 +64,7 @@ describe('ProfilesController (e2e)', () => {
   afterEach(async () => {
     await app.close();
   });
+
   describe('/profile/me (GET)', () => {
     let jwtToken: string;
     let user: UserEntity;
@@ -52,9 +72,11 @@ describe('ProfilesController (e2e)', () => {
       user = await createUser(dataSource, SAMPLE_USER);
       jwtToken = (await authService.getAuthPayload(user)).access_token;
     });
+
     afterEach(async () => {
       await deleteUserByAttribute(dataSource, { pkUserId: user.pkUserId });
     });
+
     describe('Positive Tests', () => {
       it('Should return current logged in users profile', async () => {
         // ACT
@@ -75,6 +97,7 @@ describe('ProfilesController (e2e)', () => {
         expect(res.body.email).toEqual(SAMPLE_USER.email);
       });
     });
+
     describe('Negative Tests', () => {
       it('Should not be accessible without auth', async () => {
         // ACT
@@ -97,15 +120,15 @@ describe('ProfilesController (e2e)', () => {
   describe('/profile/:userId (GET)', () => {
     let jwtToken: string;
     let user: UserEntity;
-    let creationDate: Date 
+    let creationDate: Date;
     beforeEach(async () => {
       user = await createUser(dataSource, SAMPLE_USER);
-      creationDate = SAMPLE_USER.createdAt
+      creationDate = SAMPLE_USER.createdAt;
       const loggedInUser = await createUser(dataSource, {
         username: 'abc',
         password: 'abc',
         email: 'abc@abc.abc',
-        createdAt: SAMPLE_USER.createdAt
+        createdAt: SAMPLE_USER.createdAt,
       });
       jwtToken = (await authService.getAuthPayload(loggedInUser)).access_token;
     });
@@ -116,31 +139,36 @@ describe('ProfilesController (e2e)', () => {
     describe('Positive Tests', () => {
       it('Should fetch user profile by id', async () => {
         // ACT
-        const res = await request(app.getHttpServer())
-          .get(`/profile/${user.pkUserId}`)
+        const res = await request(app.getHttpServer()).get(
+          `/profile/${user.pkUserId}`,
+        );
         // ASSERT
         expect(res.statusCode).toEqual(HttpStatus.OK);
         expect(res.body.username).toEqual(SAMPLE_USER.username);
       });
+
       it('Should ommit private field from profile', async () => {
         // ACT
-        const res = await request(app.getHttpServer())
-          .get(`/profile/${user.pkUserId}`)
+        const res = await request(app.getHttpServer()).get(
+          `/profile/${user.pkUserId}`,
+        );
         // ASSERT
         expect(res.statusCode).toEqual(HttpStatus.OK);
         expect(res.body).not.toHaveProperty('email');
       });
+
       it('Should return correct createdAt', async () => {
         // ACT
-        const res = await request(app.getHttpServer())
-          .get(`/profile/${user.pkUserId}`)
+        const res = await request(app.getHttpServer()).get(
+          `/profile/${user.pkUserId}`,
+        );
         // ASSERT
         expect(res.statusCode).toEqual(HttpStatus.OK);
-        const profile: Partial<UserEntity> =  res.body
-        expect(profile.createdAt).toEqual(creationDate.toISOString())
+        const profile: Partial<UserEntity> = res.body;
+        expect(profile.createdAt).toEqual(creationDate.toISOString());
       });
     });
-    
+
     describe('Negative Tests', () => {
       it('Should fail for invalid userId', async () => {
         // ACT
