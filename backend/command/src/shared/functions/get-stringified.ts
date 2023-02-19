b91c821846f997d7e0ed07b753aed6c10cd2a487
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-02-19 14:51:41 +0100

    cleanup(command): remove unneeded

--- backend/command/src/shared/functions/get-stringified.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @description Stringifies an object. Especially handy for objects with circular dependency
- */
-export const getStringified = (obj: any) => {
-  return JSON.stringify(obj, null, 2);
-};
