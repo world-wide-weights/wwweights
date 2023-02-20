ec9f00199b3635ba481aceb0bd562b7052bb19e0
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-02-20 14:45:55 +0100

    submission(prep): add sample data

--- db/setup/02-users.sql
+++ db/setup/02-users.sql
@@ -11,21 +11,21 @@ INSERT INTO users(
 	)
 VALUES (
 		1,
-		"GoodGradeGiver",
-		"serious@business.com",
-		"$2b$10$0DefPSo66iP32JfcsnAydO4pYjkZHLEjUoc3imgy9dgwpfhhl31qi",
-		"unverified",
-		"user",
-		"2023-02-20 11:15:18.627+00",
-		"2023-02-20 11:15:18.614+00"
+		'GoodGradeGiver',
+		'serious@business.com',
+		'$2b$10$0DefPSo66iP32JfcsnAydO4pYjkZHLEjUoc3imgy9dgwpfhhl31qi',
+		'unverified',
+		'user',
+		'2023-02-20 11:15:18.627+00',
+		'2023-02-20 11:15:18.614+00'
 	),
 	(
 		2,
-		"AdditionalUser",
-		"i.create.mock.data@entertaiment.com",
-		"$2b$10$1e5zMPXwMHN/MXzyooEH9e5L1ShuAj6sMgMkR3bNnG9QdPQCAtrp6",
-		"unverified",
-		"user",
-		"2023-02-20 11:37:38.282+00",
-		"2023-02-20 11:37:38.265+00"
+		'AdditionalUser',
+		'i.create.mock.data@entertaiment.com',
+		'$2b$10$1e5zMPXwMHN/MXzyooEH9e5L1ShuAj6sMgMkR3bNnG9QdPQCAtrp6',
+		'unverified',
+		'user',
+		'2023-02-20 11:37:38.282+00',
+		'2023-02-20 11:37:38.265+00'
 	);
